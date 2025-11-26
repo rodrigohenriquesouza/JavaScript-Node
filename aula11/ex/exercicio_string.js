0cb58dd93a47ed30f1cdbccfe547f32bfa8dbf8b
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2025-11-26 11:07:09 -0300

    .

--- aula11/ex/exercicio_string.js
+++ aula11/ex/exercicio_string.js
@@ -9,5 +9,3 @@ document.body.innerHTML += `As últimas 3 letras do seu nome são: <strong>${nom
 document.body.innerHTML += `As palavras do seu nome são: <strong>${nome.split(' ')}<strong/><br>`
 document.body.innerHTML += `Seu nome com letras maiúsculas: <strong>${nome.toUpperCase()}<strong/><br>`
 document.body.innerHTML += `Seu nome com letras minúsculas: <strong>${nome.toLowerCase()}<strong/><br>`
-
-
