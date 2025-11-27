0ec1235d0807aa2a21c879e28b9b9d6e366299cd
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2025-11-26 22:13:15 -0300

    Introdução a function

---
+++ aula16/index.js
@@ -0,0 +1,13 @@
+/*function soma(v1 = 1, v2 = 1) {
+    const resultado = v1 + v2
+    return resultado
+}
+
+const resultado = soma(4, 2)
+console.log(resultado) */
+
+const raiz = (n) => Math.sqrt(n)
+
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))
