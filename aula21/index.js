99d411b37906dad6954dca1d279d8f0db416f0df
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2025-11-28 22:50:37 -0300

    Operadores lógicos

---
+++ aula21/index.js
@@ -0,0 +1,17 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU -> Apenas uma expressão precisa ser verdadeira para retornar true
+! -> NOT -> NÃO -> Inverto um valor
+*/
+
+//const expressaoAnd = true && true && true && true
+//const expressaoOr = true || false || false || false || true
+
+//const usuario = 'Rodrigo'
+//const senha = '123456'
+
+//const vaiLogar = usuario === 'Rodrigo' && senha === '12345'
+
+
+console.log(!!true)
