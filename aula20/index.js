cc454b070d8d5a200c64f8336a5f5ea626f1f519
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2025-11-28 22:35:53 -0300

    introdução a operadores artimeticos

---
+++ aula20/index.js
@@ -0,0 +1,18 @@
+/* Lógica de programação
+
+Operadores de comparação
+> maior que
+>= maior ou igual a
+< menor que
+<= menor que ou igual a
+== iguldade (valor) **** não recomendado utilizar ****
+=== identico (valor e tipo de dado)
+!= diferente (valor) **** não recomendado utilizar ****
+!== (diferente de valor e tipo de dado)
+
+*/
+
+const num1 = 10 // number
+const num2 = '10' // string
+const comp = num1 !== num2
+console.log(comp)
