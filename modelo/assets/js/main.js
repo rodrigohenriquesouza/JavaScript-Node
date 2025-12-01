1e01120b0c28a7a02543aec3f5e912fada1a871c
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2025-11-30 22:17:11 -0300

    div de resultado

--- modelo/assets/js/main.js
+++ modelo/assets/js/main.js
@@ -3,5 +3,11 @@ const form = document.querySelector('#form')
 form.addEventListener('submit', function (event) {
     event.preventDefault() 
         console.log('Evento previnido')
+        setResultado('Olá Mundo!')
     
 }) 
+
+function setResultado(msg) {
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML = msg
+}
