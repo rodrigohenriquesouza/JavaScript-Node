f9d3ba0303ad0617b498f7adb51d3e72cea0b689
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2025-11-30 22:26:30 -0300

    Criando classes para os elementos com JS PURO

--- modelo/assets/js/main.js
+++ modelo/assets/js/main.js
@@ -11,6 +11,7 @@ function setResultado(msg) {
     const resultado = document.querySelector('#resultado')
     resultado.innerHTML = ''
     const p = document.createElement('p')
+    p.classList.add('paragrafo-resultado')
     p.innerHTML = 'Qualquer coisa'
     resultado.appendChild(p)
 }
