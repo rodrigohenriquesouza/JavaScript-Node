def1297ce4603c59554f6788ffedba673e574bdd
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2025-12-08 01:37:30 -0300

    revisando o codigo e aprimorando

--- modelo/assets/js/main.js
+++ modelo/assets/js/main.js
@@ -9,9 +9,38 @@ form.addEventListener('submit', function (event) {
     const altura = Number(inputAltura.value)
 
     if (!peso) {
-        setResultado('Peso inválido')
+        setResultado('Peso inválido', false)
+        return
     }
+
+    if (!altura) {
+        setResultado('Altura inválida', false)
+        return
+    }
+
+    const imc = getImc(peso, altura)
+    const nivelImc = getNivelImc(imc)
+
+    const msg = `Seu IMC é ${imc} ${nivelImc}`
+
+    setResultado(msg, true)
+
 }) 
+
+function getNivelImc(imc) {
+    const nivel = ['Abaixo do peso', 'Peso Normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
+
+    if (imc >= 39.9) return nivel [5]
+    if (imc >= 34.9) return nivel [4]
+    if (imc >= 29.9) return nivel [3]
+    if (imc >= 24.9) return nivel [2]
+    if (imc >= 18.5) return nivel [1]
+    if (imc < 18.5)  return nivel [0]
+}
+function getImc(peso, altura) {
+    const imc = peso / altura ** 2
+    return imc.toFixed(2)
+}
 
 function criaP () {
     const p = document.createElement('p')
@@ -19,10 +48,18 @@ function criaP () {
     
 }
 
-function setResultado(msg) {
+function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resultado')
     resultado.innerHTML = ''
 
     const p = criaP()
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado')
+    } else {
+        p.classList.add('bad')
+    }
+    p.innerHTML = msg
+    resultado.appendChild(p)
 
 }
