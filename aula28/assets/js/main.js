f669b3810682ec833576891b1fa3d4d0f1401a75
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2025-12-10 23:59:31 -0300

    exercicio date e switch

---
+++ aula28/assets/js/main.js
@@ -0,0 +1,39 @@
+const h1 = document.querySelector('.container h1')
+const data = new Date()
+
+function diaAtual(hoje) {
+
+    switch (hoje) {
+        
+        case 0: return 'Domingo'
+        case 1: return 'Segunda-feira'
+        case 2: return 'Terça-feira'
+        case 3: return 'Quarta-feira'
+        case 4: return 'Quinta-feira'
+        case 5: return 'Sexta-feita'
+        case 6: return 'Sábado'
+        default: return ''
+    }
+    
+}
+
+h1.innerHTML = diaAtual(data.getDay())
+
+/* const data = new Date()
+const hoje = data.getDay()
+const hojeTexto = diaAtual(hoje)
+
+console.log(hojeTexto) */
+
+
+
+
+
+
+
+
+
+
+
+
+
