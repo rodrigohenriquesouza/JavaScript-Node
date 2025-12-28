5817639d278e2437a7c3a7ac769380915e412ba4
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2025-12-28 16:57:50 -0300

    exercicio para datas com diferentes metodos

---
+++ aula29/assets/js/main.js
@@ -0,0 +1,43 @@
+const h1 = document.querySelector('.container h1')
+const data = new Date()
+
+function diaSemanaTexto(diaSemana) {
+    const diasSemana = ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado']
+    return diasSemana[diaSemana]
+}
+
+function getNomeMes (numeroMes) {
+    const meses = ['janeiro', 'feveiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+    return meses[numeroMes]
+}
+
+function zeroAEsquerda(num) {
+    return num >= 10 ? num : `0${num}`
+}
+
+function criaData(data) {
+    const diaSemana = data.getDay()
+    const numeroMes = data.getMonth()
+
+    const nomeDia = diaSemanaTexto(diaSemana)
+    const nomeMes = getNomeMes(numeroMes)
+
+    return (
+        `${nomeDia}, ${data.getDate()} de ${nomeMes}` +
+        ` de ${data.getFullYear()}` +
+        ` ${zeroAEsquerda(data.getHours())}:${zeroAEsquerda(data.getMinutes())}`
+    )
+}
+
+h1.innerHTML = criaData(data) 
+
+
+/* const h1 = document.querySelector('.container h1')
+const data = new Date()
+
+const opcoes = {
+    dateStyle: 'full',
+    timeStyle: 'short'
+}
+
+h1.innerHTML = data.toLocaleString('pt-BR', opcoes) */
