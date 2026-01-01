d7aa2056053c886cdcbfaecdfc6fa0b8fb3ae97d
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-01 18:02:49 -0300

    estrutura de repetição FOR

---
+++ aula33/index.js
@@ -0,0 +1,18 @@
+/* console.log('Linha 1')
+console.log('Linha 2')
+console.log('Linha 3')
+console.log('Linha 4')
+console.log('Linha 5')
+console.log('Linha 6')
+console.log('Linha 7')
+console.log('Linha 8')
+console.log('Linha 9') */
+
+// i - index
+
+const frutas = ['maça', 'abacaxi', 'pêra', 'uva']
+
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}:`, frutas[i])
+}
