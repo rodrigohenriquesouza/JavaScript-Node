c7756bd03d23a01fe443c5c4e5c86bc547e099f7
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-01 19:39:21 -0300

    exercico aula 33, varrendo vetores

---
+++ Treinos_conteudo_aula/exercicio_aula_33/index.js
@@ -0,0 +1,5 @@
+const lutadoresFavoritos = ['Charles Oliveira', 'Alex Poatan', 'Sean Strickland', 'Alexandre Pantoja', 'Max Holloway', 'Dustin Poirier']
+
+for (i = 0; i < lutadoresFavoritos.length; i++) {
+    console.log(`Lutador Favorito ${i + 1}: ${lutadoresFavoritos[i]}`)
+}
