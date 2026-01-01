10e5abc3d6e6d6b11fffba50f37b95fcd780a3cb
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-01 19:28:36 -0300

    exercicio aula 31

---
+++ Treinos_conteudo_aula/exercicio_aula_31/index.js
@@ -0,0 +1,7 @@
+const lutadores = [
+    ['Dustin Poirier', 'Charles Oliveira', 'Ilia Topuria', 'Max Holloway'], ['Anderson Silva', 'Jon Jones', 'José Aldo'], ['Sean Strikland', 'Khamzat Chimaev', 'Dricus Duplessis']
+]
+
+const [pesoLeve, ...restoLutadores] = lutadores
+
+console.log(restoLutadores)
