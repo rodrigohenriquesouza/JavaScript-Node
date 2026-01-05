0ac04738de45e32a4e71b69714cbacb5a4c41148
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-05 15:42:51 -0300

    consolidando exercicio, e voltando com tudo aos estudos

--- aula34/assets/js/main.js
+++ aula34/assets/js/main.js
@@ -1,18 +1,18 @@
-const elementos = [
-    {tag: 'p', texto: 'Testando'},
-    {tag: 'div', texto: 'Exercicio'},
-    {tag: 'section', texto: 'Novamente'},
-    {tag: 'footer', texto: 'Teste'}
+const lutadoresUFC = [
+    {tag: 'h1', lutador: 'Lutadores'},
+    {tag: 'p', lutador: 'Charles Do Bronx'},
+    {tag: 'div', lutador: 'Jon Jones'},
+    {tag: 'footer', lutador: 'Valter Walker'},
+    {tag: 'section', lutador: 'Carlos Prates'}
 ]
-
 
 const container = document.querySelector('.container')
 const div = document.createElement('div')
 
-for (i = 0; i < elementos.length; i++) {
-    let {tag, texto} = elementos[i]
+for (i = 0; i < lutadoresUFC.length; i++) {
+    let {tag, lutador} = lutadoresUFC[i]
     let tagCriada = document.createElement(tag)
-    tagCriada.innerText = texto
+    tagCriada.innerText = lutador
     div.appendChild(tagCriada)
 }
 
