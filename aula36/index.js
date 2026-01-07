babeb457c05d13b0373532c7886902728a24886c
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-07 19:53:41 -0300

    for of

---
+++ aula36/index.js
@@ -0,0 +1,23 @@
+const carro = {
+    modelo: 'BMW',
+    rodas: 4,
+    cor: 'Amarelo'
+}
+
+// for clássico - Geralmente com iteráveis (array ou strings)
+// for in - Retorna índice ou chave (string, array ou objetos)
+// for of - Retorna o valor (iteráveis, arrays ou strings)
+
+/* for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i])
+} */
+
+/*  for (let i in nomes) {
+    console.log(nomes[i])
+}
+
+console.log("##############")
+
+for (let valor of nomes) {
+    console.log(valor)
+} */
