ac7ee45195004e8bad14e5d3d28dac3763d2fb5b
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-08 20:36:59 -0300

    fazendo exercicios, e utilizando arrows functions

---
+++ Treinos_conteudo_aula/exercicio_aula_39/index.js
@@ -0,0 +1,7 @@
+// Escreva uma função que recebe 2 numeros e retorne o maior deles
+
+
+const max2 = (n1, n2) => n1 > n2 ? n1 : n2
+
+console.log(max2(100, 220))
+
---
+++ Treinos_conteudo_aula/exercicio_aula_39/index2.js
@@ -0,0 +1,8 @@
+// Escreva uma função chamada ePaisagem que recebe dois argumentos, largura e altura
+// de uma imagem (number)
+//Retorna true se a imagem estiver no modo paisagem
+
+const ePaisagem = (l, a) => l > a
+
+
+console.log(ePaisagem(1920, 1910))
