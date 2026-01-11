eda5fe3ac7eed87c11c6c7b655bf56cbcf5b9aca
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-11 15:41:54 -0300

    praticando tratamento de erros

--- aula39/index.js
+++ aula39/index.js
@@ -1,4 +1,4 @@
-try {
+/* try {
     // Executa quando não há erros
     
     console.log('Abri um arquivo')
@@ -18,4 +18,33 @@ try {
 } finally {
     // Executa sempre
     console.log('FINALLY: Eu sempre sou executado')
+} */
+
+function returnHour(data) {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instância de Date.')
+    }
+
+    if (!data) {
+        data = new Date()
+    }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    })
 }
+
+try {
+    const data = new Date('01-01-1970 12:58:12')
+    const hora = returnHour()
+    console.log(hora)
+} catch (e) {
+    // Tratar erro
+    console.log(e)
+} finally {
+    console.log('Tenha uma boa tarde.')
+}
+
