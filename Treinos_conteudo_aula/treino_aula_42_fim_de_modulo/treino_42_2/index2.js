63ebde0ac4fb1ad262cbc49f66f3f9f7a55a4e56
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-19 23:56:16 -0300

    criei a função de criar um item da tarefa, e a função da lista de tarefas

--- Treinos_conteudo_aula/treino_aula_42_fim_de_modulo/treino_42_2/index2.js
+++ Treinos_conteudo_aula/treino_aula_42_fim_de_modulo/treino_42_2/index2.js
@@ -2,3 +2,14 @@ const inputTexto = document.querySelector('.input-texto')
 const botaoTarefa = document.querySelector('.botao-tarefa')
 const listaTarefas = document.querySelector('.lista-tarefas')
 
+function criarTarefa() {
+    const tarefa = document.createElement('li')
+    return tarefa
+}
+
+
+
+function ListaDeTarefas () {
+    const tarefa = criarTarefa()
+
+}
