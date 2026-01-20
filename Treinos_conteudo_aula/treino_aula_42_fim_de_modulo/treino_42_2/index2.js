132354e3cf534e87e81560da0fd150c4533e2fe0
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-20 00:18:27 -0300

    Criei maneira de enviar a tarefa através do Enter, não apenas do click

--- Treinos_conteudo_aula/treino_aula_42_fim_de_modulo/treino_42_2/index2.js
+++ Treinos_conteudo_aula/treino_aula_42_fim_de_modulo/treino_42_2/index2.js
@@ -2,12 +2,17 @@ const input = document.querySelector('.input-texto')
 const botaoTarefa = document.querySelector('.botao-tarefa')
 const listaTarefas = document.querySelector('.lista-tarefas')
 
+document.addEventListener('keyup', function(e) {
+    if (e.key === 'Enter') {
+        if(!input.value) return
+        ListaDeTarefas(input.value)
+    }
+})
+
 botaoTarefa.addEventListener('click', function() {
     if (!input.value) return
         ListaDeTarefas(input.value)
 })
-
-
 
 function criarTarefa() {
     const tarefa = document.createElement('li')
