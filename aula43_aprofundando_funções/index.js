2b8febef0be7724f7a6e82decfb864c0dc858fd3
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-20 23:06:51 -0300

    Entendo a função first-class objects, e as functions expression, também com arrow function

--- aula43_aprofundando_funções/index.js
+++ aula43_aprofundando_funções/index.js
@@ -1,5 +1,30 @@
+// Declaração de função (Function hoisting)
+falaOi()
 function falaOi() {
     console.log('Oi')
 }
 
-falaOi()
+// Frist-class objects (Objetos de primeira classe)
+// Function expression
+const souUmDado = function() {
+    console.log('Sou um dado.')
+}
+
+souUmDado()
+
+/* 
+function executaFuncao(funcao) {
+    console.log('Vou executar sua função abaixo:')
+    funcao()
+}
+
+executaFuncao(souUmDado) */
+
+// Arrow function 
+
+const funcaoArrow = () => {
+    console.log('Sou uma arroz function')
+}
+
+funcaoArrow()
+
