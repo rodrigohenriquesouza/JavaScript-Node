8874d7472d4bf445083e0d02a841216a0a97a807
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-23 16:25:52 -0300

    entendo função dentro de função, retornando valores interior para exterior

--- aula45/index.js
+++ aula45/index.js
@@ -2,6 +2,27 @@
 // Retorna um valor
 // Termina a função
 
-function valores(a, b) {
-    return a + b
+/* function criaPessoa(nome, sobrenome) {
+    return {nome, sobrenome}
 }
+
+const p1 = criaPessoa('Luiz', 'Otávio')
+const p2 = {
+    nome: 'João',
+    sobrenome: 'Henrique'
+}
+
+console.log(typeof p1)
+console.log(typeof p2) */
+
+/* function bonecoLol(primeiroNome) {
+    function bonecoLol2(segundoNome) {
+        return primeiroNome + " " + segundoNome
+    }
+    return bonecoLol2
+}
+
+const primeiroNome = bonecoLol('Lee')
+const segundoNome = primeiroNome('Sin')
+
+console.log(segundoNome) */
