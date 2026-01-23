021fbc0d8d13011546595fa42c6da675ba0e98ff
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-23 16:36:13 -0300

    aula sobre retorno da função

--- aula45/index.js
+++ aula45/index.js
@@ -26,3 +26,17 @@ const primeiroNome = bonecoLol('Lee')
 const segundoNome = primeiroNome('Sin')
 
 console.log(segundoNome) */
+
+function criaMultiplicador(multiplicador) {
+    return function (n) {
+        return n * multiplicador
+    }
+}
+
+const duplica = criaMultiplicador(2)
+const triplica = criaMultiplicador(3)
+const quadriplica = criaMultiplicador(4)
+
+console.log(duplica(10))
+console.log(triplica(2))
+console.log(quadriplica(2))
