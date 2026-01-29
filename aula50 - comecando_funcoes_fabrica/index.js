b253c4e2ebe2b0e952659545234e02ceb6dc349e
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-29 01:37:13 -0300

    getter e setter

--- aula50 - comecando_funcoes_fabrica/index.js
+++ aula50 - comecando_funcoes_fabrica/index.js
@@ -1,15 +1,41 @@
-function criaPesso(nome, sobrenome) {
+// Factory function (Função fábrica)
+// Constructor function (Funlçao construturoa)
+function criaPesso(nome, sobrenome, altura, peso) {
     return {
         nome, 
         sobrenome,
-        fala(assunto) {
+
+        // Getter
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+        },
+
+        // Setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ')
+            this.nome = valor.shift()
+            this.sobrenome = valor.join(' ')
+            console.log(valor)
+        },
+
+        fala(assunto = 'falando sobre NADA.') {
             return `${this.nome} esta ${assunto}`
         },
-        peso: 80
+        altura: altura,
+        peso: peso,
+
+//      Getter
+        get imc() {
+            const indice = this.peso / (this.altura ** 2)
+            return indice.toFixed(2)
+        }
     }
 }
 
-const p1 = criaPesso('Rodrigo', 'Souza')
-const p2 = criaPesso('Nat', 'Oliveira')
+const p1 = criaPesso('Rodrigo', 'Souza', 1.81, 90)
+p1.nomeCompleto = 'Natalia Oliveira Souza'
+console.log(p1.nome)
+console.log(p1.sobrenome)
+console.log(p1.nomeCompleto)
+console.log(p1.fala())
 
-console.log(p2.fala('falando que irá dormir'))
