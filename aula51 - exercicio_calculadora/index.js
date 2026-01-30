620c8ac71cb8eaddc8f83c8498984dba73899fb6
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-01-30 09:15:51 -0300

    Calculadora pronta

--- aula51 - exercicio_calculadora/index.js
+++ aula51 - exercicio_calculadora/index.js
@@ -5,6 +5,15 @@ function criaCalculadora() {
 
         inicia() {
             this.cliqueBotoes()
+            this.pressionaEnter()
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', (e) => {
+                if(e.keyCode === 13) {
+                    this.realizaConta()
+                }
+            })
         },
 
         cliqueBotoes() {
@@ -22,6 +31,10 @@ function criaCalculadora() {
                 if(el.classList.contains('btn-del')) {
                     this.apagaUm()
                 }
+
+                if(el.classList.contains('btn-eq')) {
+                    this.realizaConta()
+                }
             })
         },
 
@@ -29,13 +42,33 @@ function criaCalculadora() {
             this.display.value += valor
         },
 
+        realizaConta() {
+            let conta = this.display.value
+
+            try {
+                conta = eval(conta)
+
+                if(!conta) {
+                    alert('Conta inválida')
+                    return
+                }
+
+                this.display.value = conta
+            } catch(e) {
+                alert('Conta inválida')
+                return
+            }
+        },
+
         clearDisplay() {
             this.display.value = ''
         },
 
         apagaUm() {
-            
+            this.display.value = this.display.value.slice(0, -1)
         }
+
+        
     }
 }
 
