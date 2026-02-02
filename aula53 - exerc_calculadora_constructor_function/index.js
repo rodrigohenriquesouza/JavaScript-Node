9c90ced1c25bfabf3846c0c21b850d1b4d587500
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-02 07:07:50 -0300

    treino calculadora com funcoes construtoras sozinho

--- aula53 - exerc_calculadora_constructor_function/index.js
+++ aula53 - exerc_calculadora_constructor_function/index.js
@@ -3,23 +3,64 @@ function Calculadora () {
 
     this.inicia = function () {
         this.botoesCalcu()
+        this.pressionaEnter()
     }
 
     this.botoesCalcu = function () {
-        document.addEventListener('click', function (e) {
+        document.addEventListener('click',  (e) => {
             const el = e.target
 
             if(el.classList.contains('btn-num')) {
-                valorDisplay(el.innerText)
+                this.valorDisplay(el.innerText)
+            }
+            if(el.classList.contains('btn-del')) {
+                this.apagarUm()
+            }
+            if(el.classList.contains('btn-clear')) {
+                this.limparConta()
+            }
+            if(el.classList.contains('btn-eq')) {
+                this.fazerConta()
             }
         })
      }
 
-     this.valor() = function valorDisplay(valor) {
+     Display.addEventListener('keyup', (e) => {
+        if(e.keyCode === 13) {
+            this.fazerConta()
+        }
+     })
+
+     this.fazerConta() = () => {
+        let conta = Display.value
+
+        try {
+            conta = eval(conta)
+
+            if(!conta) {
+                return 'Conta inválida'
+            }
+        } catch (erro) {
+            return 'Conta inválida'
+        }
+
+        conta = Display.value
+     }
+
+     this.valorDisplay() = (valor) => {
         Display.value += valor
+     }
+
+     this.apagarUm() = () => {
+        Display.value = Display.value.slice(0, -1)
+     }
+
+     this.limparConta() = () => {
+        Display.value = ''
      }
 
 }
 
-Calculadora.inicia()
+const calculadora = new Calculadora()
 
+calculadora.inicia()
