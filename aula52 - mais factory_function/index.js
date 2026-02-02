79824eb7f3c938e5c61e665d4157840873e9875a
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-01 22:37:44 -0300

    molde função construtora

--- aula52 - mais factory_function/index.js
+++ aula52 - mais factory_function/index.js
@@ -1,3 +1,8 @@
 // Função construtora -> objetos
 // Função fabrica -> objetos
-//
+// Construtora -> Pessoa (new)
+
+function Pessoa (nome, sobrenome) {
+    this.nome = nome
+    this.sobrenome  = sobrenome 
+}   
