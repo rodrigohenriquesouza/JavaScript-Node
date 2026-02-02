d0db26d4fd76bd85774bb43c4c1fe72cd8976dd3
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-02 06:33:14 -0300

    fim da aula factory function

--- aula52 - mais factory_function/index.js
+++ aula52 - mais factory_function/index.js
@@ -3,11 +3,23 @@
 // Construtora -> Pessoa (new)
 
 function Pessoa (nome, sobrenome) {
+    // Atributos ou métodos privados
+    const ID = 1234
+
+    const metodoInterno = function() {
+
+    }
+
+    // Atributos ou métodos públicos
     this.nome = nome
     this.sobrenome  = sobrenome 
+
+    this.metodo = function () {
+        console.log(this.nome + ': sou um metodo')
+    }
 }   
 
 const p1 = new Pessoa('Luiz', 'Otávio')
 const p2 = new Pessoa('Rodrigo', 'Souza')   
 
-console.log(typeof p1)
+p1.metodo()
