87502bdc28d69e16b949991d08c35a9747faca59
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-02 23:26:58 -0300

    pratica da revisão de arr

---
+++ aula56 - revisao_basico_em_arr/index.js
@@ -0,0 +1,10 @@
+// Valor por referência
+
+const nomes = ['Rodrigo', 'Natalia', 'Mauricio']
+const novoNome = [...nomes]
+
+const mauricio = novoNome.pop()
+
+console.log(nomes)
+console.log(novoNome)
+console.log(mauricio)
