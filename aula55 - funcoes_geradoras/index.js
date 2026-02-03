219164392333b1c990987782bb15d6660ee78814
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-02 22:31:40 -0300

    função Geradora

--- aula55 - funcoes_geradoras/index.js
+++ aula55 - funcoes_geradoras/index.js
@@ -1,6 +1,6 @@
 // Função Geradora
 
-function* geradora1 () {
+ function* geradora1 () {
     yield 1
     yield 2
     yield 3
@@ -19,3 +19,51 @@ const gerar2 = geradora2()
 console.log(gerar2.next().value)
 console.log(gerar2.next().value)
 
+ function* nomeSobrenome() {
+    yield function() {
+        console.log('Rodrigo')
+    }
+    yield function() {
+        console.log('Souza')
+    }
+}
+
+const nomeSobrenomee = nomeSobrenome()
+const nome = nomeSobrenomee.next().value
+const sobrenome = nomeSobrenomee.next().value
+
+nome()
+sobrenome()
+
+
+function* amigosFaculdade() {
+    yield function() {
+        console.log('Datan')
+    }
+    yield function() {
+        console.log('Charada')
+    }
+    yield function() {
+        console.log('Sid')
+    }
+}
+
+function* amiga() {
+    yield*amigos
+    yield function() {
+        console.log('Nat')
+    }
+}
+
+const amigos = amigosFaculdade()
+const amigaa = amiga()
+
+const amigoDatan = amigaa.next().value
+const amigoSid = amigaa.next().value
+const amigoCharada = amigaa.next().value
+const amigaNat = amigaa.next().value
+
+amigoDatan()
+amigoCharada()
+amigoSid()
+amigaNat()
