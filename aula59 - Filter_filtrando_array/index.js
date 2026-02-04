eea1bcad3619b615016cb466a75958d2cc28189c
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-04 01:30:51 -0300

    aprendendo filter

---
+++ aula59 - Filter_filtrando_array/index.js
@@ -0,0 +1,75 @@
+// Filter, map, reduce
+
+// Retorne os números maiores que 10
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+
+
+
+const numerosFiltrados = numeros.filter((valor, indice, array) => {
+    return valor > 10
+})
+console.log(numerosFiltrados)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Retorne as pessoas que tem o nome com 5 letras ou mais
+// Retorne as pessoas com mais de 50 anos
+// Retorne as pessoas cujo nome termina com a
+const pessoas = [
+    {nome: 'Rodrigo', idade: 23 },
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Natalia', idade: 17},
+    {nome: 'Mauricio', idaide: 25},
+    {nome: 'Eduarda', idaide: 55},
+    {nome: 'Wallace', idade: 47}
+]
