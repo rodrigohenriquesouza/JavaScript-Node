91d742b1c03f03c1e1441f2bdac899a037112a0d
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-04 03:23:57 -0300

    Conhecendo map() e fazendo exercicio

---
+++ aula60 - Map/index.js
@@ -0,0 +1,64 @@
+// Map - Sempre vai ter o mesmo tamanho do array original
+// Dobre os numeros
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+
+const numerosDobrados = numeros.map(n => n * 2)
+console.log(numerosDobrados)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const pessoas = [
+    {nome: 'Rodrigo', idade: 23},
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Natalia', idade: 17},
+    {nome: 'Mauricio', idade: 25},
+    {nome: 'Eduarda', idade: 55},
+    {nome: 'Wallace', idade: 47}
+]
