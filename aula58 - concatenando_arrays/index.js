3ec310dcab61303abd35d67d97ee6703f83c906a
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-04 00:00:03 -0300

    concatenando com spread operator

--- aula58 - concatenando_arrays/index.js
+++ aula58 - concatenando_arrays/index.js
@@ -1,5 +1,7 @@
 const nums1 = [1, 2, 3]
 const nums2 = [4, 5, 6]
-const juntaNums = nums1.concat(nums2, [7, 8, 9], 'Rodrigo')
+//const juntaNums = nums1.concat(nums2, [7, 8, 9], 'Rodrigo')
+// ... rest -> ... spread
+const juntaNums = [...nums1, 'Rodrigo', ...nums2, ...[7, 8 ,9]]
 
 console.log(juntaNums)
