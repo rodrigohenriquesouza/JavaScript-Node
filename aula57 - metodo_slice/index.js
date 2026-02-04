fe616078b87b801956c0cef5ade53c759314c508
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-03 23:41:38 -0300

    mais pratica e consolidado metodo splice()

--- aula57 - metodo_slice/index.js
+++ aula57 - metodo_slice/index.js
@@ -2,9 +2,23 @@ const nomes = ['Rodrigo', 'Natalia', 'Mauricio', 'Julia', 'Datan', 'Matheus']
 
 // nomes.splice(indice, quantos elementos deletar, elem1, elem2, elem3)
 
-console.log(nomes)
-
-
+/* console.log(nomes)
 
 const nomesRemovidos = nomes.splice(-5, Number.MAX_VALUE)
-console.log(nomesRemovidos)
+console.log(nomesRemovidos) */
+
+// unshift()
+/* nomes.splice(0, 0, 'Leticia')
+console.log(nomes) */
+
+// shift()
+/* nomes.splice(0, 1)
+console.log(nomes) */
+
+// push()
+/* nomes.splice(nomes.length, 0, 'Leitica', 'Julinha', 'Otavio Mesquita')
+console.log(nomes) */
+
+// pop()
+nomes.splice(-4, Number.MAX_VALUE)
+console.log(nomes)
