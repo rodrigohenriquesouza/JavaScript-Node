dd09249ceaa38ced3864a00e93ea31f713398e2a
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-05 00:25:47 -0300

    aula reduce()

---
+++ aula61 - reduce()_array/index.js
@@ -0,0 +1,16 @@
+// Some todos os numeros
+// Retorne um array com os pares (filter)
+// Retorne um array com o dobro dos valores (map)
+
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+  
+
+const pessoas = [
+     {nome: 'Rodrigo', idade: 23},
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Natalia', idade: 17},
+    {nome: 'Mauricio', idade: 25},
+    {nome: 'Eduarda', idade: 55},
+    {nome: 'Wallace', idade: 47}
+ ]
