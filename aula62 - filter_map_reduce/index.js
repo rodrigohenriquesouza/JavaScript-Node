209ff0f438cae02591b4a89532749954cc1c3cf2
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-05 17:23:49 -0300

    utilizando filter(), map() e reduce() ao mesmo tempo

---
+++ aula62 - filter_map_reduce/index.js
@@ -0,0 +1,17 @@
+// Retorne a soma do dobro de todos os pares
+// -> Filtrar pares
+// -> Dobrar os valores
+// -> Reduzir (somar tudo)
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+
+const numsPar = numeros.filter(num => num % 2 == 0).map(num => num * 2).reduce((acumulador, num) => {
+    return acumulador += num
+}, 0)
+
+console.log(numsPar)
+
+
+
+// [ 50, 80, 2, 8, 22 ]
+// [ 100, 160, 4, 16, 44 ]
