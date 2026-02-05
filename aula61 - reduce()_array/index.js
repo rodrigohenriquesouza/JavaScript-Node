7a98a02f83f954349c49b349e69e94c5e58a7ec8
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-05 16:19:14 -0300

    .

--- aula61 - reduce()_array/index.js
+++ aula61 - reduce()_array/index.js
@@ -2,10 +2,12 @@
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
   
-const total = numeros.reduce(function(acumulador, valor, indice, array) {
+const totalNpares = numeros.reduce(function(acumulador, valor, indice, array) {
     if (valor % 2 === 0) acumulador += valor
     return acumulador
 }, 0)
+
+
 
 /* pequeno teste
 const nums2 = [5, 5, 10, 7, 10]
