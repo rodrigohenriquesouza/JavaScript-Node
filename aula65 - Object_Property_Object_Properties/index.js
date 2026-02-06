ae167191af6c58c4fe79b277e2e34f8a58b3841b
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-06 10:29:09 -0300

    Entendendo e praticando com Object.defineProperty()

--- aula65 - Object_Property_Object_Properties/index.js
+++ aula65 - Object_Property_Object_Properties/index.js
@@ -2,6 +2,19 @@
 function Produto(nome, preco, estoque) {
     this.nome = nome
     this.preco = preco
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // mostra a chave 
+        value: estoque, // valor
+        writable: true, // altera ou não
+        configurable: false // configurável
+    })
 }
 
-const p1 = new Produto()
+const p1 = new Produto('Luvas MMA', 230, 15)
+
+p1.estoque = 25
+
+for(let chave in p1) {
+    console.log(p1[chave])
+}
