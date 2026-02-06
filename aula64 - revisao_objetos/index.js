00c3c04c15eb632d0e8bec86fe0ebaf1c7427459
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-06 08:38:21 -0300

    revisao objeto, e metodos (funcoes dentro de objetos)

--- aula64 - revisao_objetos/index.js
+++ aula64 - revisao_objetos/index.js
@@ -6,3 +6,19 @@
 
     const profissao = 'profissao'
     console.log(pessoa[profissao]) */
+
+const p1 = new Object()
+
+p1.nome = 'Rodrigo'
+p1.sobrenome = 'Souza'
+p1.idade = 23
+p1.falaProfissao = function () {    
+    return `${this.nome} ${this.sobrenome}, trabalha como programador`
+}
+p1.getDataDeNascimento = function () {
+    const data = new Date()
+    return 'Nasceu em: ' + (data.getFullYear() - this.idade)
+}
+
+console.log(p1.falaProfissao())
+console.log(p1.getDataDeNascimento())
