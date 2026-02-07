3cbd7249f42e96e7df064513b6ed536c7defca60
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-07 12:33:45 -0300

    pratica com validação de dados com setter, e factory function

--- aula66 - Getters_Setters/index.js
+++ aula66 - Getters_Setters/index.js
@@ -1,21 +1,44 @@
+
+// pratica com Constructor function
 function ProdutosMMA(produto, preco, estoque) {
     this.produto = produto
     this.preco = preco
+
+    let estoqueLoja = estoque
 
     Object.defineProperty(this, 'estoque', {
         enumerable: true,
         configurable: true,
         get: function() {
-            return estoque
+            return estoqueLoja
         },
         set: function(qtd) {
             
             if (typeof qtd !== 'number') {
                 throw new TypeError('Aceita somento numeros')
             }
-            estoque = qtd
+            estoqueLoja = qtd
         }
     })
 }
 
-const luva = new ProdutosMMA('luva', 200, 10)
+const luva = new ProdutosMMA('luva', 200, 10) 
+
+luva.estoque = 20
+console.log(luva.estoque)
+
+// pratica com Factory function
+function camisas(cor) {
+    return {
+        get cor() {
+            return cor
+        },
+        set cor(tipoDeCor) {
+            if (typeof tipoDeCor !== 'string') throw new TypeError('Escreva a cor da camisa')
+            cor = tipoDeCor
+        }
+}
+}
+
+const camisaAzul = camisas('Azul escuro')
+camisaAzul.cor = 'Vermelha'
