dd5d85d5aa3bd632de4ade01037dc784cb1b6024
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-07 18:25:02 -0300

    pratica da aula

--- auta67 - Métodos_uteis_para_objetos/index.js
+++ auta67 - Métodos_uteis_para_objetos/index.js
@@ -1,4 +1,23 @@
-const caneta = {nome: 'caneta', preco: 0.5}
-const lapis = {nome: 'lapis', preco: caneta.preco}
-console.log(caneta)
-console.log(lapis)
+/* 
+Object.values
+Object.entries
+Object.getOwnPropertyDescriptor(objeto, 'propriedade')
+Object.assign(objeto destino, any)
+... (spread)
+*/
+
+// Ja vimos
+// Object.keys() - vejo as chaves do meu obj
+// Object.freeze() - congelo meu objeto, assim ele não consegue receber mudanças
+// Object.definerProperties() - define varias propriedades, enumerable, writable, configurable, value
+// Object.defineProperty() - define uma propriedade, enumerable, writable, configurable, value
+
+const produto = {nome: 'Saco de pancada', preco: 200}
+
+
+
+for(let valor of Object.values(produto)) {
+    console.log(valor)
+}
+
+
