cb7e576e97b04e294386a418a3b04cf468ee36ca
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-09 09:47:26 -0300

    pratica exercicio validando cpf

--- aula71 - Exercício - Validando_um_cpf_Algoritmo/index2.js
+++ aula71 - Exercício - Validando_um_cpf_Algoritmo/index2.js
@@ -14,3 +14,51 @@ Se o numero digito for maior que 9, consideramos 0.
 11 - (284 % 11) = 2 (Primeiro dígito)
 Se o número digito for maior que 9 consideramos 0.
 */
+
+function ValidarCPF(cpf) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        enumerable: true,
+        get: function() {
+            return cpf.replace(/\D+/g, '')
+        }
+    })
+}   
+
+ValidarCPF.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false
+    if(this.cpfLimpo.length !== 11) return false
+    if(this.eSequencia()) return false
+
+    const cpfCortado = this.cpfLimpo.slice(0, -2)
+    const digito1 = this.comparaDigito(cpfCortado)
+    const digito2 = this.comparaDigito(cpfCortado + digito1)
+    const cpfCompleto = cpfCortado + digito1 + digito2
+
+    return cpfCompleto === this.cpfLimpo
+}
+
+ValidarCPF.prototype.eSequencia = function() {
+    return this.cpfLimpo[0].repeat(this.cpfLimpo.length) === this.cpfLimpo
+}
+
+ValidarCPF.prototype.comparaDigito = function(cpfCortado) {
+    const arrayCPF = Array.from(cpfCortado)
+    let multiplicaRegressivo = arrayCPF.length + 1
+    const total = arrayCPF.reduce((acml, valor) => {
+        acml += (multiplicaRegressivo * Number(valor))
+        multiplicaRegressivo--
+        return acml
+    }, 0)
+
+    const digito = 11 - (total % 11)
+    return digito > 9? '0': String(digito)
+}
+
+const cpf = new ValidarCPF('482.085.428.30')
+
+if(cpf.valida()) {
+    console.log('VALIDO')
+} else {
+    console.log('CPF INVALIDO')
+}
+
