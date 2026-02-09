b152c1df0c8925fd429a9f3d491eda659fb2427a
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-08 21:49:02 -0300

    Entendendo heranças de prototypes e constructor,

--- aula70 - Herança/index.js
+++ aula70 - Herança/index.js
@@ -1,2 +1,29 @@
 // Produto
-// Camiseta, caneca, lapis
+// Camiseta, caneca
+
+function Produto(nome, valor) {
+    this.nome = nome
+    this.valor = valor
+}
+
+Produto.prototype.aumento = function (aumento) {
+    this.valor += aumento
+}
+
+Produto.prototype.desconto = function(desconto) {
+    this.valor -= desconto
+}
+
+function Camiseta(nome, valor, cor) {
+    Produto.call(this, nome, valor)
+    this.cor = cor
+}
+
+Camiseta.prototype = Object.create(Produto.prototype)
+Camiseta.prototype.constructor = Camiseta
+
+const produtoQualquer = new Produto('Luva-Boxe', 300)
+const camisetaRoxa = new Camiseta('Camisa', 100, 'Roxa')
+console.log(produtoQualquer)
+camisetaRoxa.aumento(20)
+console.log(camisetaRoxa)
