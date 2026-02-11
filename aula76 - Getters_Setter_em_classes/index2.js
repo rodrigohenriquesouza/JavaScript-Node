ef9e8eea14ba8b969f7a70ba82774f2be4578635
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-11 07:37:44 -0300

    Mais prticas com getter e setter com class

--- aula76 - Getters_Setter_em_classes/index2.js
+++ aula76 - Getters_Setter_em_classes/index2.js
@@ -1,1 +1,25 @@
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
+
+    set nomeCompleto(nome) {
+        nome = nome.split(' ')
+        this.nome = nome.shift()
+        this.sobrenome = nome.join(' ')
+    }
+
+    get nomeCompleto() {
+        return this.nome + ' ' + this.sobrenome
+    }
+
+}
+
+const p1 = new Pessoa('Rodrigo', 'Souza')
+const p2 = new Pessoa('Natlia', 'Oliveira')
+p1.nomeCompleto = 'Rodrigo Henrique Souza'
+p2.nomeCompleto = 'Natailia Oliveira Souza'
+
+
 
