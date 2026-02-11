7a6382405cc9c9148455a46c024a3d7a84876444
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-10 23:14:07 -0300

    metodos de acelerar e freiar o carro criados

--- aula76 - Getters_Setter_em_classes/index.js
+++ aula76 - Getters_Setter_em_classes/index.js
@@ -1,1 +1,18 @@
-    
+class Carro {
+    constructor(nomeCarro, velocidade) {
+        this.nomeCarro = nomeCarro
+        this.velocidade = 0
+    }
+
+    acelerar() {
+        if(this.velocidade >= 100) return
+        this.velocidade++
+    }
+
+    desacelerar() {
+        if(this.desacelerar <= 0) return
+        this.velocidade--
+    }
+}
+
+const carro1 = new Carro()
