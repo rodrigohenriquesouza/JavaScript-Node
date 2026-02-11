152cd5af8e1f991c2c6226a1dc3edca036cc1884
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-11 01:08:35 -0300

    mexendo com get e settt na class

--- aula76 - Getters_Setter_em_classes/index.js
+++ aula76 - Getters_Setter_em_classes/index.js
@@ -1,18 +1,36 @@
+const _vel = Symbol()
 class Carro {
-    constructor(nomeCarro, velocidade) {
+    constructor(nomeCarro) {
         this.nomeCarro = nomeCarro
-        this.velocidade = 0
+        this[_vel] = 0
     }
+
+    set velocidade(velocidade) {
+        if (typeof velocidade !== 'number') return
+        if (velocidade >= 100 || velocidade <= 0) return
+        console.log('setter')
+        this[_vel] = velocidade
+    }
+
+    get velocidade() {
+        return this[_vel]
+    }   
+
+    
 
     acelerar() {
-        if(this.velocidade >= 100) return
-        this.velocidade++
+        if(this[_vel] >= 100) return
+        this[_vel]++
     }
 
-    desacelerar() {
-        if(this.desacelerar <= 0) return
-        this.velocidade--
+    freiar() {
+        if(this[_vel] <= 0) return
+        this[_vel]--
     }
 }
 
-const carro1 = new Carro()
+const carro1 = new Carro('Camaro')
+
+carro1.acelerar()
+carro1.velocidade = 60
+console.log(carro1.velocidade)
