dc82afba43de7297960c35d766e2fac94a963471
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-12 14:32:16 -0300

    criado o evento de submit, e o metodo que ira lidar com ele

--- aula80 - Validando_um_formulario_classes/assets/js/pratica_main.js
+++ aula80 - Validando_um_formulario_classes/assets/js/pratica_main.js
@@ -3,6 +3,12 @@ class ValidaForm {
         this.formulario = document.querySelector('.form')
         this.eventos()
     }
+
+    eventos() {
+        this.formulario.addEventListener('submit', e => {
+            this.handleSubmit(e)
+        })
+    }
 }
 
 const form = new ValidaForm()
