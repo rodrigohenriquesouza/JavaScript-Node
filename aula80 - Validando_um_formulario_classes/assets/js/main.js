d7784d5f37189f7232dfb115501009ebac7d72cf
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-12 13:00:53 -0300

    refatorando a visualização dos erros do formulario

--- aula80 - Validando_um_formulario_classes/assets/js/main.js
+++ aula80 - Validando_um_formulario_classes/assets/js/main.js
@@ -19,8 +19,11 @@ class ValidaFormulario {
         let valid = true
 
         for(let campo of this.formulario.querySelectorAll('.validar')) {
+            const label = campo.previousElementSibling.innerText
+
             if(!campo.value) {
-                this.criaErro(campo, `Campo tal não pode estar em branco.`)
+                this.criaErro(campo, `Campo "${label}" não pode estar em branco.`)
+                valid = false
             }
         }
     }
