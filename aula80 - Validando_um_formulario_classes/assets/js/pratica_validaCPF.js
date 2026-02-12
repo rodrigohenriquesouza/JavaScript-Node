7e6a002deab517fe702191d135bc92c0f9bbf567
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-12 15:08:06 -0300

    criando metodo para pegar digitos no validador de CPF

--- aula80 - Validando_um_formulario_classes/assets/js/pratica_validaCPF.js
+++ aula80 - Validando_um_formulario_classes/assets/js/pratica_validaCPF.js
@@ -7,6 +7,20 @@ class ValidaCPF {
             }
         })
     }
+
+    valida() {
+        if(typeof this.cpfLimpo !== 'string') return false
+        if(this.cpfLimpo.length > 11) return false
+
+        const cpfFatiado = this.cpfLimpo.slice(0, -2)
+        const digito1 = this.pegaDigitos(cpfFatiado)
+        const digito2 = this.pegaDigitos(cpfFatiado)
+    }
+
+    pegaDigitos(cpfFatiado) {
+        const arrayCPF = Array.from(cpfFatiado)
+    }
 }
 
-const cpf = new ValidaCPF()
+const cpf = new ValidaCPF('482.085.428.30')
+cpf.valida()
