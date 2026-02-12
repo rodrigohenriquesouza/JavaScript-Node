6bf30c6bef89f662354d8ebf93503dc9a319500c
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-12 15:56:40 -0300

    metodo para validar usuario criado

--- aula80 - Validando_um_formulario_classes/assets/js/pratica_main.js
+++ aula80 - Validando_um_formulario_classes/assets/js/pratica_main.js
@@ -13,12 +13,15 @@ class ValidaForm {
     handleSubmit(e) {
         e.preventDefault()
         const validaInputs = this.inputsValids()
+        const validaSenha
     }
 
     inputsValids() {
         let valid = true
 
-       
+       for(let errorText of this.formulario.querySelectorAll('.text-error')) {
+        errorText.remove()
+       }
 
         for(let input of this.formulario.querySelectorAll('.validar')) {
             const label = input.previousElementSibling.innerText
@@ -31,8 +34,25 @@ class ValidaForm {
             if(input.classList.contains('cpf')) {
                 if(!this.validaCPF(input)) valid = false
         }   
+
+            if(input.classList.contains('usuario')) {
+                if(!this.validUser(input)) valid = false
+            }
     }
 }
+
+    validUser(input) {
+        let valid = true
+        if(input.value.length < 3 || input.value.length > 12) {
+            this.throwError(input, 'Usuário deve conter de 3 a 12 caracteres') 
+            valid = false
+        }
+        if(!input.value.match(/^[a-zA-Z0-9]+$/)) {
+            this.throwError(input, 'Usuário deve conter apenas letras e/ou numeros')
+            valid = false
+        }
+
+    }
 
     validaCPF(input) {
         const cpf = new ValidaCPF(input.value)
