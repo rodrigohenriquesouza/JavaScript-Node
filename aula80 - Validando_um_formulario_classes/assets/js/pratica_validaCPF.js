7ebd9b22744fc9b4fc6a5e083119a30696fc833a
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-12 15:02:43 -0300

    testando get() de cpf limpo dentro da classe

--- aula80 - Validando_um_formulario_classes/assets/js/pratica_validaCPF.js
+++ aula80 - Validando_um_formulario_classes/assets/js/pratica_validaCPF.js
@@ -1,6 +1,11 @@
 class ValidaCPF {
     constructor(cpf) {
-        
+        Object.defineProperty(this, 'cpfLimpo', {
+            enumerable: true,
+            get() {
+                return cpf.replace(/\D+/g, '')
+            }
+        })
     }
 }
 
