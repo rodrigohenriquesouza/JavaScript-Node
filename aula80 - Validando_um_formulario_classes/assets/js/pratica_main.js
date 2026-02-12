f202eacbc59bbd567742b798c4da956734d8b75c
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-12 16:31:48 -0300

    codigo comentado

--- aula80 - Validando_um_formulario_classes/assets/js/pratica_main.js
+++ aula80 - Validando_um_formulario_classes/assets/js/pratica_main.js
@@ -1,28 +1,28 @@
-class ValidaForm {
+class ValidaForm {       // class para validar formulario
     constructor() {
-        this.formulario = document.querySelector('.form')
-        this.eventos()
+        this.formulario = document.querySelector('.form')   //seleciono meu form com DOM
+        this.eventos()         // crio meu evento assim que chamo a class
     }
 
-    eventos() {
-        this.formulario.addEventListener('submit', e => {
+    eventos() {                                                          // adiciono meu evento de submit
+        this.formulario.addEventListener('submit', e => {      
             this.handleSubmit(e)
         })
     }
-
-    handleSubmit(e) {
+  
+    handleSubmit(e) {                                          // lida com meu evento e o que fazer depois dele ser acionado
         e.preventDefault()
         const inputsValidados = this.inputsValids()
         const senhasValidadas = this.passwordValids()
 
-        if(inputsValidados && senhasValidadas) {
+        if(inputsValidados && senhasValidadas) {               // compara se os inputs e as senhas são validas para dar submit
             alert('Formulario feito sozinho enviado!')
             this.formulario.submit()
         }
     }
 
-    passwordValids() {
-        let valid = true
+    passwordValids() {                                        // valida minha senha do formulario
+        let valid = true                                                          
 
         const senha = document.querySelector('.senha')
         const repeteSenha = document.querySelector('.repetir-senha')
@@ -40,26 +40,26 @@ class ValidaForm {
         return valid
     }
 
-    inputsValids() {
-        let valid = true
+    inputsValids() {                         // valida meus inputs do formulario (campos) Nome/Sobrenome/CPF
+        let valid = true         // flag
 
        for(let errorText of this.formulario.querySelectorAll('.text-error')) {
         errorText.remove()
        }
 
-        for(let input of this.formulario.querySelectorAll('.validar')) {
-            const label = input.previousElementSibling.innerText
+        for(let input of this.formulario.querySelectorAll('.validar')) {        // itera sobre cara class ".validar" e seleciona os inputs
+            const label = input.previousElementSibling.innerText                // seleciona o elemento irmão anterior ao input e pega o que da escrito nele e atribui a variavel "label"
 
-            if(!input.value) {
-                valid = false
-                this.throwError(input, `O ${label} precisa ser preenchido!.`)
+            if(!input.value) {                                                            // lança o erro de campos vazios
+                this.throwError(input, `O ${label} precisa ser preenchido!.`)    
+                valid = false   
             }
 
-            if(input.classList.contains('cpf')) {
+            if(input.classList.contains('cpf')) {                           // lança o erro caso o CPF não seja valido
                 if(!this.validaCPF(input)) valid = false
         }   
 
-            if(input.classList.contains('usuario')) {
+            if(input.classList.contains('usuario')) {                     // lança o erro caso o Usuario não atenda as condicionais
                 if(!this.validUser(input)) valid = false
             }
     }
@@ -67,20 +67,20 @@ class ValidaForm {
 }
 
     validUser(input) {
-        let valid = true
-        if(input.value.length < 3 || input.value.length > 12) {
+        let valid = true            // frag
+        if(input.value.length < 3 || input.value.length > 12) {                   // compara o tamanho do input
             this.throwError(input, 'Usuário deve conter de 3 a 12 caracteres') 
             valid = false
         }
-        if(!input.value.match(/^[a-zA-Z0-9]+$/)) {
+        if(!input.value.match(/^[a-zA-Z0-9]+$/)) {                                      // verifica se o valor do input de Usuário contém apenas números e letras
             this.throwError(input, 'Usuário deve conter apenas letras e/ou numeros')
             valid = false
         }
-        return valid
+        return valid       // retorna a flag
     }
 
-    validaCPF(input) {
-        const cpf = new ValidaCPF(input.value)
+    validaCPF(input) {                                          // metodo que valida o CPF
+        const cpf = new ValidaCPF(input.value)              // chama minha class ValidaCPF
 
         if(!cpf.valida()) {
             this.throwError(input, 'CPF inválido')
@@ -89,12 +89,12 @@ class ValidaForm {
         return true
     }
 
-    throwError(input, msg) {
+    throwError(input, msg) {                             // metodo que lança os erros
         const divMsg = document.createElement('div')
         divMsg.innerHTML = msg
-        divMsg.classList.add('text-error')
-        input.insertAdjacentElement('afterend', divMsg)
+        divMsg.classList.add('text-error')        // adiciona class a div
+        input.insertAdjacentElement('afterend', divMsg)      // insere ao element adjacente, no parametro "afterend" depois do input, insere a "divMsg" que seria a mensagem de erro
     }
 }
 
-const form = new ValidaForm()
+const form = new ValidaForm() // variavel atribuida a class
