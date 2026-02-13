0a9fb8f4c18f4ba9ad9307a8a78686a257ee5e2a
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-13 15:06:53 -0300

    metodo de Promise.all()

--- aula82 - Métodos_uteis_para_Promisses/index.js
+++ aula82 - Métodos_uteis_para_Promisses/index.js
@@ -8,11 +8,28 @@ function randomNumber(min, max) {
 function esperaAi(msg, tempo) {
     return new Promise((resolve, reject) => {
         if(typeof msg !== 'string') {
-            return new Error('NÃO É STRING')
+            return reject(new Error('Erro'))
         }
 
         setTimeout(() => {
-
+            resolve(msg.toUpperCase() + '- Passei na promisse')
         }, tempo)
     })
 }
+
+// Promise.all Promise.race Promise.resolve Promise.reject
+
+const promises = ['XXXXX',
+    esperaAi('Promise 1', randomNumber(1, 3)),
+    esperaAi('Promise 2', randomNumber(1, 3)),
+    esperaAi('Promise 3', randomNumber(1, 3)),
+    'XXXXX'
+]
+
+Promise.all(promises)
+.then(valor => {
+    console.log(valor)
+})
+.catch(erro => {
+    console.log(erro)
+})
