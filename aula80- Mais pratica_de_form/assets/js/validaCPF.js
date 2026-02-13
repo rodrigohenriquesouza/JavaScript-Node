69309e8d845b1a42f5b862f88758b14207d9b612
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-13 11:31:56 -0300

    criado a class que validaCPF

--- aula80- Mais pratica_de_form/assets/js/validaCPF.js
+++ aula80- Mais pratica_de_form/assets/js/validaCPF.js
@@ -8,18 +8,33 @@ class ValidaCPF {
     }
 
     valid() {
+        if(typeof this.cpfCleam !== 'string') return false
+        if(this.cpfCleam.length !== 11) return false
+        if(this.sequential()) return console.log(`CPF fake`)
+
         const cpfSlice = this.cpfCleam.slice(0, -2)
         const digito1  = this.takeDigits(cpfSlice) 
         const digito2 = this.takeDigits(cpfSlice + digito1)
+        const cpfComplete = cpfSlice + digito1 + digito2
+
+        return cpfComplete === this.cpfCleam
     }
 
     takeDigits(cpfSlice) {
         let total = 0
-        let regressive = 0
+        let regressive = cpfSlice.length + 1
 
         for(let dig of cpfSlice) {
-
+            total += Number(dig) * regressive
+            regressive--
         }
+        
+        const dig = 11 - (total % 11)
+        return dig > 9? '0' : String(dig)
+    }
+
+    sequential() {
+        return this.cpfCleam[0].repeat(this.cpfCleam.length) === this.cpfCleam
     }
 }
 
