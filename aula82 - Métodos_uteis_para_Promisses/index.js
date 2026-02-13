c96431c80784f3a9681251106e3a7b57c46e8af5
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-13 14:44:21 -0300

    começo da aula

--- aula82 - Métodos_uteis_para_Promisses/index.js
+++ aula82 - Métodos_uteis_para_Promisses/index.js
@@ -1,1 +1,18 @@
-function randomNumber()
+function randomNumber(min, max) {
+    min *= 1000
+    max *= 1000
+
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        if(typeof msg !== 'string') {
+            return new Error('NÃO É STRING')
+        }
+
+        setTimeout(() => {
+
+        }, tempo)
+    })
+}
