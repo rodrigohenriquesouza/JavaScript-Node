1918eaef55ac1ab2f5e4389f9be5b2a3f932ef27
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-14 13:51:25 -0300

    entendendo funcionamento do http

--- aula84 - XMLHttpRequest_(GET)_+_Promises/assets/js/main.js
+++ aula84 - XMLHttpRequest_(GET)_+_Promises/assets/js/main.js
@@ -1,22 +1,46 @@
-const requetes = obj => {
-    const xhr = new XMLHttpRequest()
-    xhr.open(obj.method, obj.url, true)
-    xhr.send()
+const request = obj => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(obj.method, obj.url, true);
+    xhr.send();
 
     xhr.addEventListener('load', () => {
-        if(xhr.status >= 200 && xhr.status < 300) {
-            obj.sucess(xhr.responseText)
-        } else {
-            obj.error(xhr.statusText)
-        }
-    })
-}
+      if(xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.responseText);
+      } else {
+        reject(xhr.statusText);
+      }
+    });
+  });
+};
 
 document.addEventListener('click', e => {
-    const el = e.target
-    const tag = el.tagName.toLowerCase()
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
 
-    if(tag === 'a') {
-        
-    }
-})
+  if (tag === 'a') {
+    e.preventDefault();
+    carregaPagina(el);
+  }
+});
+
+async function carregaPagina(el) {
+  const href = el.getAttribute('href');
+
+  const objConfig = {
+    method: 'GET',
+    url: href
+  };
+
+  try {
+    const response = await request(objConfig);
+    carregaResultado(response);
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+function carregaResultado(response) {
+  const resultado = document.querySelector('.resultado');
+  resultado.innerHTML = response;
+}
