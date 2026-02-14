d2b53086160f44c9cf5a6929b2e932d7b06f403f
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-14 08:19:02 -0300

    mais pratica asyn  / await

--- aula83 - Async_Await/index.js
+++ aula83 - Async_Await/index.js
@@ -38,13 +38,25 @@ function esperaAi(msg, tempo) {
 async function executaPromise() {
     try {
     
-     const resultados = await Promise.all([
+        const ranking = await esperaAi('Ranking', rand())
+        console.log(ranking)
+
+        const rank1 = await esperaAi('1', rand())
+        console.log(rank1)
+
+        const rank2 = await esperaAi('2', rand())
+        console.log(rank2)
+
+        const rank3 = await esperaAi('3', rand())
+        console.log(rank3)
+        
+/*      const resultados = await Promise.all([
         esperaAi('TESTE 1', rand()),
         esperaAi('TESTE 2', rand()),
         esperaAi('TESTE 3', rand())
         ])
         
-        console.log(resultados)
+        console.log(resultados) */
     } catch(erro) {
         console.log(erro)
     }
