597b7f0739a528da02988a16e43b23a7b969c4b4
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-16 18:15:27 -0300

    criada função para ver os dados json dentro da pagina

--- aula86 - fetch_axios/assets/js/main.js
+++ aula86 - fetch_axios/assets/js/main.js
@@ -3,9 +3,26 @@ fetch('pessoas.json')
 .then(json => carregaJsonNaPag(json))
 
 function carregaJsonNaPag(json) {
+  const table = document.createElement('table')
   for(pessoa of json) {
-    console.log(pessoa.nome)
+    let tr = document.createElement('tr')
+
+    let td = document.createElement('td')
+    td.innerHTML = pessoa.nome
+    tr.appendChild(td)
+
+    let td2 = document.createElement('td')
+    td2.innerHTML = pessoa.idade
+    tr.appendChild(td2)
+
+    let td3 = document.createElement('td')
+    td3.innerHTML = pessoa.salario
+    tr.appendChild(td3)
+
+    table.appendChild(tr)
   }
+  const res = document.querySelector('.resultado')
+  res.appendChild(table)
 }
 
 carregaJsonNaPag()
