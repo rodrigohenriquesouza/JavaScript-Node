a20a4f5d29e487d67046f5687ed0b8d622cc0b56
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-16 18:02:39 -0300

    criado função para mostrar json na tela

--- aula86 - fetch_axios/assets/js/main.js
+++ aula86 - fetch_axios/assets/js/main.js
@@ -2,3 +2,9 @@ fetch('pessoas.json')
 then(resposta => resposta.json())
 then(json => carregaJsonNaPag(json))
 
+function carregaJsonNaPag(json) {
+  const div = document.querySelector('.resultado')
+  div.innerHTML = json
+}
+
+carregaJsonNaPag()
