ebee31426c22b2007c5d411e002e290db6030a4e
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-16 18:04:04 -0300

    refatorando codigo

--- aula86 - fetch_axios/assets/js/main.js
+++ aula86 - fetch_axios/assets/js/main.js
@@ -1,10 +1,11 @@
 fetch('pessoas.json')
-then(resposta => resposta.json())
-then(json => carregaJsonNaPag(json))
+.then(resposta => resposta.json())
+.then(json => carregaJsonNaPag(json))
 
 function carregaJsonNaPag(json) {
-  const div = document.querySelector('.resultado')
-  div.innerHTML = json
+  for(pessoa of json) {
+    console.log(pessoa.nome)
+  }
 }
 
 carregaJsonNaPag()
