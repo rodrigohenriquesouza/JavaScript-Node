1392b83050d2e639614032db7ede3b1fbbf425e4
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-17 11:36:59 -0300

    ambiente configurado para futuras aulas

---
+++ aula88-web_package/public/assets/js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// 1
+// 2
+// 3
+console.log('HEEEEEEy');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
