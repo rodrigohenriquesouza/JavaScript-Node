4f1e7ee6669ee7900cd729b715360156c48b1cee
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-18 12:25:01 -0300

    importante uma função construtora

--- aula89-es6_modules_import_export/src/index.js
+++ aula89-es6_modules_import_export/src/index.js
@@ -1,6 +1,9 @@
-import { nome as nome2, sobrenome, idade, soma} from './modulo1'
+import { nome as nome2, sobrenome, idade, soma, Pessoa as PessoaQlq} from './modulo1'
 
 const nome = 'Miranda'
 
-console.log(nome2, sobrenome, idade, amor)
+console.log(nome2, sobrenome, idade)
 console.log(soma(5, 5))
+
+const p1 = new PessoaQlq('Souza', 'Menegel')
+console.log(p1)
--- aula89-es6_modules_import_export/src/modulo1.js
+++ aula89-es6_modules_import_export/src/modulo1.js
@@ -2,6 +2,13 @@ export const nome = 'Rodrigo'
 export const sobrenome = 'Souza'
 export const idade = 23
 
+export class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
+}
+
 
 export function soma(a, b) {
     return a + b
