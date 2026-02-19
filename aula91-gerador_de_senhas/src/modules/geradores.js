d6ee4d4922a08dea222e52c2e573d5055f8bc25c
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-19 12:08:11 -0300

    function gera numeros

--- aula91-gerador_de_senhas/src/modules/geradores.js
+++ aula91-gerador_de_senhas/src/modules/geradores.js
@@ -1,5 +1,7 @@
 const rand = (min, max) => Math.floor(Math.random() * (max - min) + min)
 const geraMaiscula = () => String.fromCharCode(rand(65, 91))
 const geraMinusculas = () => String.fromCharCode(rand(97, 123))
+const geraNumero = () => String.fromCharCode(rand(48, 58))
 console.log(geraMaiscula())
 console.log(geraMinusculas())
+console.log(geraNumero())
