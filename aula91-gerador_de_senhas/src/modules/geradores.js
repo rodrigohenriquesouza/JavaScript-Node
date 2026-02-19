88438b8f7bfeff6fb3ff1b2206abb806963a64b4
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-19 12:11:18 -0300

    gera simbolos

--- aula91-gerador_de_senhas/src/modules/geradores.js
+++ aula91-gerador_de_senhas/src/modules/geradores.js
@@ -2,6 +2,9 @@ const rand = (min, max) => Math.floor(Math.random() * (max - min) + min)
 const geraMaiscula = () => String.fromCharCode(rand(65, 91))
 const geraMinusculas = () => String.fromCharCode(rand(97, 123))
 const geraNumero = () => String.fromCharCode(rand(48, 58))
+const simbolos = '.,/?^~`´[&%]\|#@!(*{}_'
+const geraSimbolo = () => simbolos[rand(0, simbolos.length)]
 console.log(geraMaiscula())
 console.log(geraMinusculas())
 console.log(geraNumero())
+console.log(geraSimbolo())
