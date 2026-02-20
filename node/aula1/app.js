a8b55e042ce3a98dbd9922b0352bbb469f8e6e2b
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-20 16:50:57 -0300

    praticando export e import com node. module.exports / require()

---
+++ node/aula1/app.js
@@ -0,0 +1,5 @@
+const mod1 = require('./mod1')
+const falaNome = mod1.falaNome
+
+console.log(falaNome())
+
--- node/aula1/mod1.js
+++ node/aula1/mod1.js
@@ -1,7 +1,13 @@
 const nome = 'Rodrigo'
 const sobrenome = 'Souza'
 
-const falaNome = () => {
-    console.log(nome, sobrenome)
-}
-falaNome()
+const falaNome = () => nome + ' ' + sobrenome
+
+
+/* module.exports.nome = nome
+module.exports.sobrenome = sobrenome
+module.exports.falaNome = falaNome */
+
+exports.nome = nome
+exports.sobrenome = sobrenome
+exports.falaNome = falaNome
