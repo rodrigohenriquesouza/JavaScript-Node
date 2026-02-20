50f043a0f2744525fb40d2c09e28ca4ab001167a
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-20 16:53:49 -0300

    importando via desestruturação

--- node/aula1/app.js
+++ node/aula1/app.js
@@ -1,5 +1,11 @@
-const mod1 = require('./mod1')
+const { nome, sobrenome, falaNome} = require('./mod1')
+
+console.log(nome)
+console.log(falaNome())
+
+
+/* const mod1 = require('./mod1')
 const falaNome = mod1.falaNome
 
-console.log(falaNome())
+console.log(falaNome()) */
 
