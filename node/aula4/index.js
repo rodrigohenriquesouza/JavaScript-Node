81e1542786f1e3c78e6a7c39f5d1dd95253762fd
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-21 19:57:00 -0300

    pratica da aula

--- node/aula4/index.js
+++ node/aula4/index.js
@@ -1,7 +1,12 @@
 const fs = require('fs').promises
 const path = require('path')
 
-fs.readdir('./')
+async function readdir(rootDir) {
+    rootDir = rootDir || path.resolve(__dirname)
+    const files = await fs.readdir()
+}
+
+/* fs.readdir(path.resolve(__dirname))
     .then(files => console.log(files))
     .catch(e => console.log(e))
-
+ */
