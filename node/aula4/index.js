61daecaf25e2e5551a1fbd0007007a223c392387
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-22 21:58:27 -0300

    função que "caminha" nos arquivos

--- node/aula4/index.js
+++ node/aula4/index.js
@@ -3,9 +3,17 @@ const path = require('path')
 
 async function readdir(rootDir) {
     rootDir = rootDir || path.resolve(__dirname)
-    const files = await fs.readdir()
+    const files = await fs.readdir(rootDir)
+    walk(files)
 }
 
+function walk(files) {
+    for(let file of files) {
+        console.log(file)
+    }
+}
+
+readdir()
 /* fs.readdir(path.resolve(__dirname))
     .then(files => console.log(files))
     .catch(e => console.log(e))
