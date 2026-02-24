20bf2e181d38d1e4d7f91d12976959cecec61b12
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-23 21:27:31 -0300

    criado metodo para limpar o cpf

--- aula79 - Usando_classes_validandoCPF/indexvalida3.js
+++ aula79 - Usando_classes_validandoCPF/indexvalida3.js
@@ -1,7 +1,15 @@
 class validadorCPF {
     constructor(cpf) {
-        Object.defineProperties()
-    }
+        Object.defineProperty(this, 'cpfLimpado', {
+            get() {
+                return cpf.replace(/\D+/g, '')
+            }
+        })
+}
+
+    veRepeticao()
 }
 
 const cpf1 = new validadorCPF('482085428.30')
+
+console.log(cpf1.cpfLimpado)
