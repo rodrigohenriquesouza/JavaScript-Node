3b3b0b25787de85317489f7859a0bdc2adb08e08
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-26 15:15:25 -0300

    lendo com async e function

--- node/aula4/index.js
+++ node/aula4/index.js
@@ -1,6 +1,10 @@
 const fs = require('fs').promises
 const path = require('path')
 
-fs.readdir(path.resolve(__dirname))
-    .then(files => console.log(files))
-    .catch(e => console.log(e))
+async function leDiretorios() {
+
+    const files = await fs.readdir(path.resolve(__dirname))
+    console.log(files)
+}
+
+leDiretorios()
