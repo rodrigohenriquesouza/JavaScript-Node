dc1fbbd4b70bfad98cdcc7980bf3e3c95a1c8f5d
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-26 15:00:08 -0300

    entendendo o FS "file system" e path "caminho"

--- node/aula4/index.js
+++ node/aula4/index.js
@@ -1,27 +1,4 @@
-const fs = require('fs').promises
+const fs = require('fs')
 const path = require('path')
 
-async function readdir(rootDir) {
-    rootDir = rootDir || path.resolve(__dirname)
-    const files = await fs.readdir(rootDir)
-    walk(files, rootDir)
-}
-
-async function walk(files, rootDir) {
-    for(let file of files) {
-        const fileFullPath = path.resolve(rootDir, file)
-        const stats = await fs.stat(fileFullPath)
-        console.log(file, stats.isDirectory())
-
-        if(stats.isDirectory()) {
-            readdir(fileFullPath)
-            continue
-        }
-    }
-}
-
-readdir(path.resolve('E:/estudos/JavaScript-Node'))
-/* fs.readdir(path.resolve(__dirname))
-    .then(files => console.log(files))
-    .catch(e => console.log(e))
- */
+console.log(__dirname)
