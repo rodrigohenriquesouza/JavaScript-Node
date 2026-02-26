16f058054c0d8fe6170e883d52accf9ab679d38b
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-02-26 17:38:49 -0300

    Iterando sob todos os diretorios e vendo os arquivos dentro deles

--- node/aula4/index.js
+++ node/aula4/index.js
@@ -11,6 +11,11 @@ async function walkFiles(files, rootDir) {
     for(let file of files) {
     const fileFullPath = path.resolve(rootDir, file)
     const stats = await fs.stat(fileFullPath)
+
+    if(stats.isDirectory()) {
+        leDiretorios(fileFullPath)
+    }
+
         console.log(file, stats.isDirectory())
     }
 }
