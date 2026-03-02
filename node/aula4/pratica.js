35a5614ea08a5c49a393a6b9984be161d66afaf0
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-03-01 23:54:56 -0300

    função que le o diretorio

--- node/aula4/pratica.js
+++ node/aula4/pratica.js
@@ -1,6 +1,7 @@
-const files = require('fs').promises
+const fs = require('fs').promises
 const path = require('path')
 
 async function leDiretorios(rootDir) {
 rootDir = rootDir || path.resolve(__dirname)
+const files = await fs.readdir(rootDir)
 }
