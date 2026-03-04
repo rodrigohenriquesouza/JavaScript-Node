f56dab191cd23ab32a699c0045306e27267f85b1
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-03-03 23:16:49 -0300

    praticando mais com file system e caminhos

--- node/aula4/pratica.js
+++ node/aula4/pratica.js
@@ -1,26 +1,3 @@
 const fs = require('fs').promises
 const path = require('path')
 
-async function leDiretorios(rootDir) {
-rootDir = rootDir || path.resolve(__dirname)
-const files = await fs.readdir(rootDir)
-procuraArquivo(files, rootDir)
-}
-
-async function procuraArquivo(files, rootDir) {
-    for(let file of files) {
-        const caminhoArquivo = path.resolve(rootDir, file)
-        const status = await fs.stat(caminhoArquivo)
-
-        if(status.isDirectory()) {
-            leDiretorios(caminhoArquivo)
-            continue
-        }
-        if(!/.css$/g.test(caminhoArquivo) && !/.html$/g.test(caminhoArquivo)) {
-            continue
-        }
-        caminhoArquivo
-    }
-}
-
-leDiretorios('E:/estudos/JavaScript-Node')
