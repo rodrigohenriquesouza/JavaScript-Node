0432d488e91d432e7039b16161356494825bb117
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-03-03 23:57:39 -0300

    procurando diretorios com recursividade

--- node/aula4/pratica.js
+++ node/aula4/pratica.js
@@ -1,7 +1,7 @@
 const fs = require('fs').promises
 const path = require('path')
 
-async function arquivos(rootDir) {
+async function diretorios(rootDir) {
     rootDir = rootDir || path.resolve(__dirname)
     const arquivos = await fs.readdir(rootDir)
     procuraArquivo(arquivos, rootDir)
@@ -10,5 +10,18 @@ async function arquivos(rootDir) {
 async function procuraArquivo(arquivos, rootDir) {
     for(let arquivo of arquivos) {
         const caminhoArquivo = path.resolve(rootDir, arquivo)
+        const stats = await fs.stat(caminhoArquivo)
+
+        if(stats.isDirectory()) {
+            diretorios(caminhoArquivo)
+            continue
+        }
+
+        if(!/.css$/g.test(caminhoArquivo) && !/.html$/g.test(caminhoArquivo)) {
+            continue
+        }
+        console.log(caminhoArquivo)
     }
 }
+
+diretorios('E:/estudos/JavaScript-Node')
