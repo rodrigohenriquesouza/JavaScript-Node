a504b2b9b1640344527d2728a1b7074b788d2ba5
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-03-03 23:41:24 -0300

    pega o caminho completo de cada arquivo dentro do diretorio

--- node/aula4/pratica.js
+++ node/aula4/pratica.js
@@ -6,3 +6,9 @@ async function arquivos(rootDir) {
     const arquivos = await fs.readdir(rootDir)
     procuraArquivo(arquivos, rootDir)
 }
+
+async function procuraArquivo(arquivos, rootDir) {
+    for(let arquivo of arquivos) {
+        const caminhoArquivo = path.resolve(rootDir, arquivo)
+    }
+}
