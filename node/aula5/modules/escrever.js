264fa44df3945b90a74dc33257811ccbe5e4f885
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-03-05 16:50:18 -0300

    transformando um objeto em JSON

--- node/aula5/modules/escrever.js
+++ node/aula5/modules/escrever.js
@@ -1,5 +1,14 @@
 const fs = require('fs').promises
 const path = require('path')
-const caminhoArquivo = path.resolve(__dirname, '..', 'teste.txt')
+const caminhoArquivo = path.resolve(__dirname, '..', 'teste.json')
 
-fs.writeFile(caminhoArquivo, 'Frase 1', { flag: 'a', encoding: 'utf8'})
+const pessoas = [
+    {nome: 'João'},
+    {nome: 'Maria'},
+    {nome: 'Eduardo'},
+    {nome: 'Stephanie'}
+]
+
+const json = JSON.stringify(pessoas, '', 2)
+
+fs.writeFile(caminhoArquivo, json, { flag: 'w', encoding: 'utf8'})
