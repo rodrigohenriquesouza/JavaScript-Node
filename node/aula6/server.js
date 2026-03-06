1b65a2038261c693ed15dc5324be778b08262a86
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-03-06 02:20:03 -0300

    entendendo express e usando os metdoso de get, post, e send()

--- node/aula6/server.js
+++ node/aula6/server.js
@@ -3,3 +3,29 @@ const app = express()
 
 // CRUD -> CREATE, READ, UPDATE, DELETE
 //          POST   GET    PUT    delete
+
+// http://meusite.com/ <- GET -> Entrega a página
+// http://meusite.com/sobre <- GET -> Entrega a página/sobre
+// http://meusite.com/contato <- GET -> Entrega a página/contato
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form action="/" method="POST">
+        Nome: <input type="text" name="nome">
+        <button>Enviar</button
+        </form>
+        `)
+})
+
+app.post('/', (req, res) => {
+    res.send('Recebi formulario')
+})
+
+app.get('/contato', (req, res) => {
+    res.send('Você entrou em contatos!')
+})
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000')
+    console.log('Servidor executando na porta 3000')
+})
