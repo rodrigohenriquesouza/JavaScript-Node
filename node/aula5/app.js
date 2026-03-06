b409209ee7a545ea38072db5837b41154de13fc0
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-03-06 00:41:46 -0300

    funções para ler os arquivos, aula para escrever e ler arquivo com node.js

--- node/aula5/app.js
+++ node/aula5/app.js
@@ -1,8 +1,9 @@
 const path = require('path')
 const caminhoArquivo = path.resolve(__dirname, 'teste.json')
 const escreve = require('./modules/escrever')
+const le = require('./modules/ler')
 
-const pessoas = [
+/* const pessoas = [
     {nome: 'João'},
     {nome: 'Maria'},
     {nome: 'Eduardo'},
@@ -10,4 +11,16 @@ const pessoas = [
 ]
 
 const json = JSON.stringify(pessoas, '', 2)
-escreve(caminhoArquivo, json)
+escreve(caminhoArquivo, json) */
+
+async function leFile(caminho) {
+    const dados = await le(caminho)
+    renderizaDados(dados)
+}
+
+function renderizaDados(dados) {
+    dados = JSON.parse(dados)
+    dados.forEach(val => console.log(val))
+}
+
+leFile(caminhoArquivo)
