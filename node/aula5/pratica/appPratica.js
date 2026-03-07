2141a4efd579c1fdafa8466eb2e265e7cf4c4285
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-03-07 17:18:29 -0300

    feito e escrito arquivo json ficticio

--- node/aula5/pratica/appPratica.js
+++ node/aula5/pratica/appPratica.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const caminho = path.resolve(__dirname, 'praticandoNode.json')
-
+const escreveArquivo = require('./modules/escreverPratica')
 
 const dadosPessoas = [
     {nome: 'Julia'},
@@ -8,3 +8,7 @@ const dadosPessoas = [
     {nome: 'Natalia'},
     {nome: 'Datan'}
 ]
+
+const json = JSON.stringify(dadosPessoas, '', 2)
+
+escreveArquivo(caminho, json)
