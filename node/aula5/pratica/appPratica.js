69e4eb426a3375d4dc4b949555ae13fbbeabe458
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-03-07 17:16:00 -0300

    acessando diretorio atual e criando um documento json e dados ficticios

--- node/aula5/pratica/appPratica.js
+++ node/aula5/pratica/appPratica.js
@@ -1,2 +1,10 @@
 const path = require('path')
-const caminho = 
+const caminho = path.resolve(__dirname, 'praticandoNode.json')
+
+
+const dadosPessoas = [
+    {nome: 'Julia'},
+    {nome: 'Rodrigo'},
+    {nome: 'Natalia'},
+    {nome: 'Datan'}
+]
