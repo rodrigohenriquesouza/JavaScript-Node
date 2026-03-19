1aa661cd44f11d52f1b016d914d6a34ecafde656
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-03-18 23:17:37 -0300

    leve revisão

--- node/aula7/index.js
+++ node/aula7/index.js
@@ -3,3 +3,17 @@ const voltaEstudo = 'volta'
 for(let i = 0; i <= 10; i++) {
     console.log('Vou virar um programador')
 }
+
+class geraPessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
+
+    falaOi() {
+        console.log(`${this.nome} Oi`)
+    }
+}
+
+const eu = new geraPessoa('Rodrigo', 'Souza')
+eu.falaOi()
