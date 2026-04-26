50bdc9d9c1f88d1630c56f114bb550c131ca0de8
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-04-26 16:51:46 -0300

    insere valor no display

--- aula51 - exercicio_calculadora/index3.js
+++ aula51 - exercicio_calculadora/index3.js
@@ -1,11 +1,26 @@
 class Calculadora {
     constructor() {
-
+        this.display = document.querySelector('.display')
     }
 
     inicia() {
-        alert('Oi iniciei')
+        this.cliqueBotoes()
+    }
+
+    cliqueBotoes() {
+        document.addEventListener('click', function(e) {
+            const el = e.target
+
+            if(el.classList.contains('btn-num')) {
+                this.btnParaDisplay(el.innerText)
+            }
+        }.bind(this))
+    }
+
+    btnParaDisplay(valor) {
+        this.display.value += valor
     }
 }
 
 const calculadora = new Calculadora()
+calculadora.inicia()
