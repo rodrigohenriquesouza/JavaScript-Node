de730fd6b62e4b28371734f3ced57d758a94b276
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-06-01 21:08:54 -0300

    metodo que verifica se o numero do CPF se repete

--- aula71 - Exercício - Validando_um_cpf_Algoritmo/index3.js
+++ aula71 - Exercício - Validando_um_cpf_Algoritmo/index3.js
@@ -5,10 +5,23 @@ class validaUmCpf {
         Object.defineProperty(this, 'cpfLimpo', {
             writable: false,
             configurable: false,
-            enumerable:false
+            enumerable: true,
             value: cpf.replace(/\D+/g, '')
         })
+    }
+
+    verificaSequencia() {
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo
+    }
+
+    valida() {
+        if(!this.cpfLimpo) return false
+        if(typeof this.cpfLimpo !== 'string') return false
+        if(this.cpfLimpo.length !== 11) return false
+        if(this.verificaSequencia()) return false
+        return 'CHEGUEI AQUI'
     }
 }
 
 const validaCpf = new validaUmCpf('482.085.428.30')
+console.log(validaCpf.valida())
