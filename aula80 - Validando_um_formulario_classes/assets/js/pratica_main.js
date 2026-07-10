46bcc9d865b5419e2c751e1d5b6562ce0804742d
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2026-07-09 22:51:36 -0300

    lança erro do cpf

--- aula80 - Validando_um_formulario_classes/assets/js/pratica_main.js
+++ aula80 - Validando_um_formulario_classes/assets/js/pratica_main.js
@@ -18,6 +18,10 @@ class FormValid {
     fieldChecked() {
         let valid = true
 
+        for(let throwErro of this.formulario.querySelectorAll('.throw-erro')) {
+            throwErro.remove()
+        }
+
         for(let field of this.formulario.querySelectorAll('.test')) {
             let label = field.previousElementSibling.innerText
 
@@ -25,10 +29,23 @@ class FormValid {
                 this.throwError(field, `Msg erro ${label}`)
                 valid = false
             }
-        }
 
-        return valid
-    }
+            if(field.classList.contains('cpf')) {
+                if(!this.validCpf(field)) valid = false
+            }
+        }
+  }
+
+  validCpf(field) {
+    const cpf = new ValidaCPF(field.value)
+
+    if(!cpf.valida()) {
+        this.throwError(field, 'CPF INVÁLIDO')
+        return false
+    }  
+
+    return true
+  }
 
     throwError(field, msg) {
         const div = document.createElement('div')
